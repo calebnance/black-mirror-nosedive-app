32eed991ea8158134297f9cb3399032e33cfb6b0
Caleb Nance <calebnance@gmail.com>
2016-10-31 14:19:08 -0400

    more rating functionality added

--- scripts/app.js
+++ scripts/app.js
@@ -1,10 +1,19 @@
 angular.module('nosedive', []);
 
 // init
-$('.nosedive').height($(window).height()).width($(window).width());
-$('.stars').width($(window).width());
+var height = $(window).height();
+var width = $(window).width();
+$('.nosedive').height(height);
+// $('.stars').width($(window).width());
 
 // set rating grid array
+var ratingGrid = [];
+var ratioRatin = width / 5;
+for(var i=1; i <= 5; i++) {
+	ratingGrid.push(ratioRatin * (i - 1));
+}
+
+console.log(ratingGrid);
 
 // turn off touch when swiping with finger
 window.blockMenuHeaderScroll = false;
@@ -24,29 +33,37 @@ $(window).on('touchmove', function(e) {
 	}
 });
 
+// on star click
+$('[data-rate]').on('click', function(e){
+	updateRating($(this).attr('data-rate'));
+});
+
 // swipe all the things!!1
 $('.nosedive').swipe({
-		swipe:function(event, direction, distance, duration, fingerCount, fingerData) {
-			console.log('direction');
-			console.log(direction);
-			console.log('distance');
-			console.log(distance);
-			console.log('duration');
-			console.log(duration);
-			console.log('fingerCount');
-			console.log(fingerCount);
-			console.log('fingerData');
-			console.log(fingerData);
-
-			if(direction === 'right') {
-				console.log('right');
-				$('.fa-star').addClass('star-rated');
+	swipe:function(event, direction, distance, duration, fingerCount, fingerData) {
+		if(direction === 'right') {
+			var rating = 0;
+			for(var r=0; r < 5; r++) {
+				if(ratingGrid[r] <= distance) {
+					rating = r + 1;
+				}
 			}
+			updateRating(rating);
+		}
 
-			if(direction === 'left') {
-				$('.fa-star').removeClass('star-rated');
-			}
-		},
-		threshold: 2,
-		fingers: 'all'
-	});
+		if(direction === 'left') {
+			$('.fa-star').removeClass('star-rated');
+		}
+	},
+	threshold: 2,
+	fingers: 'all'
+});
+
+// functions
+function updateRating(rating) {
+	console.log(rating);
+	$('.fa-star').removeClass('star-rated');
+	for(var s=1; s <= rating; s++) {
+		$('[data-rate="' + s + '"]').addClass('star-rated');
+	}
+}
