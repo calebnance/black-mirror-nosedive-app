7a8aa4f7dac4af261030999bb4b329b8fbd5e4c8
Caleb Nance <calebnance@gmail.com>
2018-06-17 00:45:14 -0400

    start of the re-creation

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+
+import star from './svgs/star.svg';
+import profileImg from './images/user1.jpg';
+
 import './App.css';
 
 class App extends Component {
@@ -7,9 +10,23 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
+          <div className="App-username">
+            <span>&#123;Username&#125;</span>
+            would you like to
+          </div>
+          <div className="App-user-img">
+            <img src={profileImg} alt="user image" />
+          </div>
+          <div className="App-user-to-rate">
+            Rate <span>&#123;Username&#125;</span>
+          </div>
         </header>
-        <div class="Rating-container">
-          
+        <div className="Rating-container">
+          <img src={star} alt="rating 1" />
+          <img src={star} alt="rating 2" />
+          <img src={star} alt="rating 3" />
+          <img src={star} alt="rating 4" />
+          <img src={star} alt="rating 5" />
         </div>
       </div>
     );
